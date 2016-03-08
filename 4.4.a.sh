e327c38d380f1dbee103130025bf25f779630539
dmi3y <lapshukov@gmail.com>
2016-03-07 23:10:27 -0800

    first commit

---
+++ 4.4.a.sh
@@ -0,0 +1,11 @@
+args=$#           # Number of args passed.
+lastarg=${!args}
+echo $args
+echo $lastarg
+echo
+# Note: This is an *indirect reference* to $args ...
+
+
+# Or:       lastarg=${!#}             (Thanks, Chris Monson.)
+# This is an *indirect reference* to the $# variable.
+# Note that lastarg=${!$#} doesn't work.
